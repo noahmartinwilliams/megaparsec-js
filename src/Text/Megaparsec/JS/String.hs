c5e112002a1e0bd7bcbd60de6dbf0ddfa09bde06
Noah Williams <noahmartinwilliams@gmail.com>
2026-06-05 15:44:20 -0400

    Add Single Quoted String Parsing

--- src/Text/Megaparsec/JS/String.hs
+++ src/Text/Megaparsec/JS/String.hs
@@ -7,8 +7,17 @@ import qualified Text.Megaparsec.Char.Lexer as L
 import Text.Megaparsec.JS.Space
 import Text.Megaparsec.JS.Types
 
-jsStringLit :: JSParser Expr
-jsStringLit = do
+jsStringLit1 :: JSParser Expr
+jsStringLit1 = do
     void $ (single '"')
     content <- scn1 (manyTill (char '\\' *> char '"' <|> noneOf "\"") (char '"'))
     return (StringLitExpr content)
+
+jsStringLit2 :: JSParser Expr
+jsStringLit2 = do
+    void $ (single '\'')
+    content <- scn1 (manyTill (char '\\' *> char '\'' <|> noneOf "'") (char '\''))
+    return (StringLitExpr content)
+
+jsStringLit :: JSParser Expr
+jsStringLit = (jsStringLit1 <|> jsStringLit2)
